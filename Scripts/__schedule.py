faeab4f8dba4654d7597c8429b12a3bacb5de936
aanam20 <aahmed20@usf.edu>
2023-11-30 19:36:38 -0500

    linked schedule to database for teacher

--- Scripts/__schedule.py
+++ Scripts/__schedule.py
@@ -1,15 +1,24 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
+from Scripts import azsqldb
+
+#1 add a database function to get all the assignments already associated with the class
+#  display that as a table in show_schedule
+# add a function to add new assignments
+
+
 
 def teacher_schedule():
     # Initialize session state variables
     if 'schedule_data' not in st.session_state:
-        st.session_state.schedule_data = pd.DataFrame(columns=['Task', 'Due Date'])
+        st.session_state.schedule_data = azsqldb.get_assignments(st.session_state.class_info['class_id'], st.session_state.sqlcursor)
 
-    def add_task(task, due_date):
-        new_entry = {'Task': task, 'Due Date': due_date}
-        st.session_state.schedule_data = st.session_state.schedule_data.append(new_entry, ignore_index=True)
+    def add_task(task, due_date, class_id):
+        azsqldb.add_assignment(st.session_state.sqlcursor, task, due_date, st.session_state.class_info['class_id'])
+        #new_entry = {'Task': task, 'Due Date': due_date}
+        st.session_state.schedule_data = azsqldb.get_assignments(st.session_state.class_info['class_id'], st.session_state.sqlcursor)
+        # azsqldb.add_assignment(assignment_name, due_date, class_id)
 
     def show_schedule():
         st.table(st.session_state.schedule_data)
@@ -20,7 +29,7 @@ def teacher_schedule():
         submit_button = st.form_submit_button("Add Task")
 
     if submit_button and task:
-        add_task(task, due_date)
+        add_task( task, due_date, st.session_state.class_info['class_id'])
 
     show_schedule()
 
@@ -30,11 +39,7 @@ def teacher_schedule():
 def student_schedule():
     # Initialize session state variables
     if 'schedule_data' not in st.session_state:
-        # Sample data, replace this with actual data from the teacher's input
-        st.session_state.schedule_data = pd.DataFrame([
-            {'Task': 'Read Chapter 1', 'Due Date': '2023-12-01'},
-            {'Task': 'Homework #2', 'Due Date': '2023-12-05'}
-        ])
+        st.session_state.schedule_data = azsqldb.get_assignments(st.session_state.class_info['class_id'], st.session_state.sqlcursor)
 
     if 'completed_tasks' not in st.session_state:
         st.session_state.completed_tasks = []
